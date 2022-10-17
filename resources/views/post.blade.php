712568d198dc5eadd002c12b7ea8064ae0f81437
fammyos <fammy.203040040@mail.unpas.ac.id>
2022-10-17 22:49:50 +0700

    menyelesaikan tugas video 4 laravel

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>FammyOS | Blog</title>
-</head>
-<body>
-    <h1>Halaman Blog</h1>
-   
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+<article>
+    <h2>{{ $post["title"] }}</h2>
+    <h5>{{ $post["autor"] }}</h5>
+    <p>{{ $post["Body"] }}</p>
+</article>
+
+<a href="/blog">Back</a>
+@endsection
