8ed3cd30c27871bd2a9b6f0e608d43c8fb44a24e
fammyos <fammy.203040040@mail.unpas.ac.id>
2022-11-05 23:38:24 +0700

    menyelesaikan tugas video 17 laravel

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -22,27 +22,34 @@
           </ul>
 
           <ul class="navbar-nav ms-auto">
-            @auth
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Welcome Back, {{ auth()->user()->name }}
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i>My Dashboard</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i>Logout</a></li>
-              </ul>
-            </li>
-            @else
-            <li class="nav-item">
-              <form action="/logout" method="post">
-                @csrf
-                <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i>Logout</a></button>
-              </form>
-                <a href="/login" class="nav-link" {{ ($active === 'logincategories')? 'active' : '' }} ><i class="bi bi-box-arrow-right"></i>Login</a>
-            </li>
-            @endauth
-          </ul>
+                    @auth
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Welcome Back, {{ auth()->user()->name }}
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="/dashboard"><i
+                                            class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li>
+                                    <form action="/logout" method="post">
+                                        @csrf
+                                        <button type="submit" class="dropdown-item"><i
+                                                class="bi bi-box-arrow-right"></i>Logout</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
+                        <li class="nav-item ms-auto">
+                            <a href="/login" class="nav-link {{ $active == 'login' ? 'active' : '' }}"><i
+                                    class="bi bi-box-arrow-in-right"></i> Login</a>
+                        </li>
+                    @endauth
+                </ul>
 
         </div>
       </div>
